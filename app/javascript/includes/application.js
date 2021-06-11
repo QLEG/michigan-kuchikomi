d7a75465922a69dcc722d59a2d5d4658052597e3
QLEG <qlegnb@gmail.com>
2021-06-12 01:09:57 +0900

    Update help page with JQuery

--- app/javascript/includes/application.js
+++ app/javascript/includes/application.js
@@ -1,4 +1,18 @@
 $(function() {
   $('.jumbotron').fadeIn();
   
+  $('.faq-list-item').click(function() {
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')) { 
+      $answer.removeClass('open');
+      $answer.slideUp();
+      $(this).find('span').text('+');
+    } else {
+      $answer.addClass('open'); 
+      $answer.slideDown();
+      $(this).find('span').text('-');
+      
+    }
+  });
 });
+
