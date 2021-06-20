fc2b2fd0e3de85cf9fcb102ad3975ec4382fbdb8
QLEG <qlegnb@gmail.com>
2021-06-21 01:13:39 +0900

    Revise test codes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_many :comments
   has_many :posts
   has_many :communities, through: :subscriptions
-  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
