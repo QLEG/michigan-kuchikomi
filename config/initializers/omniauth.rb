eef9a652c60ecf89588b4d478ac3ad966e08caa9
QLEG <qlegnb@gmail.com>
2021-06-23 23:30:40 +0900

    Add Twitter login feature #82

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter ,Rails.application.credentials.dig(twitter, ENV['TWITTER_KEY']), Rails.application.credentials.dig(ENV['TWITTER_SECRET'])
+provider :twitter, Rails.application.credentials.twitter[:TWITTER_KEY], Rails.application.credentials.twitter[:TWITTER_SECRET]
 end
